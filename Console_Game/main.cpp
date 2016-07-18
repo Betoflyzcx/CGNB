910cc5a16c9f68e7098c2112d0a699e7a935e8df
alber <alber@192.168.0.9>
2016-07-17 20:35:03 -0700

    I'm Here as well

---
+++ Console_Game/main.cpp
@@ -0,0 +1,48 @@
+/* 
+ * File:   main.cpp
+ * Author: Alberto Garcia
+ * Created on July 14, 2016, 4:27 PM
+ * Purpose: Console Game with NeBeans IDE
+ */
+
+ // System Libraries
+#include <iostream> // Input/Output Library
+#include <string> // Include string Libraries
+#include <cmath> // Include Math Libraries
+
+using namespace std; //Namespace of the System Libraries
+
+ //User Libraries
+
+
+
+ //Global Constants
+
+
+
+ //Function Prototypes
+void MapDraw(int x,int y);
+
+
+// Execution Begins Here!
+int main(int argc, char** argv) {
+   //Declare variables
+    int x;
+    int y;
+    cin >>x; 
+    cin >>y;
+    
+    
+    MapDraw(x,y); //Maps Length and Width
+    
+
+
+
+    //Exit Stage Right!
+    return 0;
+}
+
+void MapDraw(int x,int y)
+{
+ 
+}
