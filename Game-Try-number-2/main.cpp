436bf35313e230a094f3149e049de52cd7bfee05
fernando garcia <fernando garcia>
2016-07-18 23:27:36 -0700

    Added some minr details
    Signed-off-by: fernando garcia <fernando garcia>

--- Game-Try-number-2/main.cpp
+++ Game-Try-number-2/main.cpp
@@ -18,11 +18,31 @@ using namespace std; //Namespace of the System Libraries
 
 //Function Prototypes
 
+void CheckTurn(int turn);
+void DrawMap(int x, int y);
+
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
+	CheckTurn(1);
+	CheckTurn(2);
+	DrawMap(5, 5);
 
-
-	// Exit Stage Right!
+	//Exit Stage Right!
 	return 0;
+}
+void DrawMap(int x, int y)
+{
+	for (int i = 0; i < y; i++)
+	{
+		for (int j = 0; j < x; j++)
+		{
+			cout << "x ";
+		}
+		cout << endl;
+	}
+}
+void CheckTurn(int turn)
+{
+	cout << "Player " << turn << "'s turn" << endl; //write the current players turn
 }
