5f9abbda98afc1a20771a2df45a2f81d3d697270
alberto garcia <alberto garcia>
2016-07-18 23:19:06 -0700

    Signed-off-by: alberto garcia <alberto garcia> Started the setup

--- Game-Try-number-2/main.cpp
+++ Game-Try-number-2/main.cpp
@@ -1,1 +1,28 @@
-#include <iostream>
+/*
+* File:   main.cpp
+* Author: Alberto Garcia
+* Created on July 14, 2016, 1:00 PM
+* Purpose : Display A List Of Numbers
+*/
+
+//System Libraries
+#include <iostream> //Input/Output Library
+using namespace std; //Namespace of the System Libraries
+ //User Libraries
+
+
+
+//Global Constants
+
+
+
+//Function Prototypes
+
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+
+
+	// Exit Stage Right!
+	return 0;
+}
